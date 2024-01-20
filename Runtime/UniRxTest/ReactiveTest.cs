b7ea04fc8016d7ac0180962c3a3b64d9c195882b
Achim Bunke <achim.bunke@googlemail.com>
2024-01-20 10:46:28 +0100

    Test dependency

---
+++ Runtime/UniRxTest/ReactiveTest.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UniRx;
+using UnityEngine;
+public static class ReactiveTest 
+{
+   public static void Test<T>(this IReactiveProperty<T> r)
+    {
+
+    }
+}
