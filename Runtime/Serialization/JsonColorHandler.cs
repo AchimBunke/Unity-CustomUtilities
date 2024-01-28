484682c1f72412cf37e25708723a897dc3c0310a
Achim Bunke <achim.bunke@googlemail.com>
2024-01-28 23:12:13 +0100

    Create JsonColorHandler.cs

---
+++ Runtime/Serialization/JsonColorHandler.cs
@@ -0,0 +1,45 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace UnityUtilities.Serialization
+{
+    /// <summary>
+    /// Using https://medium.com/@altaf.navalur/serialize-deserialize-color-objects-in-unity-1731e580af94
+    /// </summary>
+    public class JsonColorHandler : JsonConverter
+    {
+        public JsonColorHandler()
+        {
+        }
+
+        public override bool CanConvert(Type objectType)
+        {
+            return true;
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            try
+            {
+                ColorUtility.TryParseHtmlString("#" + reader.Value, out Color loadedColor);
+                return loadedColor;
+            }
+            catch (Exception ex)
+            {
+                Debug.LogError($"Failed to parse color {objectType} : {ex.Message}");
+                return null;
+            }
+        }
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            string val = ColorUtility.ToHtmlStringRGB((Color)value);
+            writer.WriteValue(val);
+        }
+    }
+}
