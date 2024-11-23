e506e94c8ec0cedf2a6dde32046b67b3d797ac97
Achim Bunke <achim.bunke@googlemail.com>
2024-11-23 16:47:13 +0100

    Update TileInitializationSystem.cs

--- Runtime/Entities/Hybrid/Tilemaps/TileInitializationSystem.cs
+++ Runtime/Entities/Hybrid/Tilemaps/TileInitializationSystem.cs
@@ -1,5 +1,4 @@
-﻿#if USE_ENTITIES_1_3_5
-#if USE_PHYSICS_1_3_5
+﻿#if USE_ENTITIES_1_3_5 && USE_PHYSICS_1_3_5 && UNITY_PHYSICS_CUSTOM
 
 using System.Collections.Generic;
 using Unity.Collections;
@@ -122,5 +121,4 @@ namespace UnityUtilities.Entities.Hybrid.Tilemaps
         }
     }
 }
-#endif
 #endif
